24acc045b2647517267a2fa6fc4f57e509feab48
amelki <240f0c53eac357cd1ff1c02f9d05e376d407dc5e>
2017-04-17 09:53:58 +0200

    Newline before subscription

--- test/test_strip.js
+++ test/test_strip.js
@@ -15,11 +15,11 @@ Promise.all([
 	const strip1 = strip(customers[0], true);
 	assert.equal('https://storage.googleapis.com/senders-images/cards/stripe.png', strip1.icon);
 	assert.equal(strip1.link, 'https://dashboard.stripe.com/customers/cus_AT0MyKnpQdHOXG');
-	assert.equal(strip1.text, 'Customer added 4 days ago. \nSubscribed to Abonnement Premium annuel - active - 48 € /year. Ends on Apr 13, 2018');
+	assert.equal(strip1.text, 'Customer added 4 days ago. \n\nSubscribed to Abonnement Premium annuel - active - 48 € /year. Ends on Apr 13, 2018');
 	const strip2 = strip(customers[1], true);
 	assert.equal('https://storage.googleapis.com/senders-images/cards/stripe.png', strip2.icon);
 	assert.equal(strip2.link, 'https://dashboard.stripe.com/customers/cus_AT0MyKnpQdHOXG');
-	assert.equal(strip2.text, 'Customer added 4 days ago. \nSubscribed to Abonnement Premium annuel - active - 48 € /year');
+	assert.equal(strip2.text, 'Customer added 4 days ago. \n\nSubscribed to Abonnement Premium annuel - active - 48 € /year');
 	console.log('Test OK');
 }).catch(err => {
 	console.log(util.inspect(err));
